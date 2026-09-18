f22d7db62fae536356d5fdc1f4bfe9a11095e269
unknown <6910122113026@pnru.ac.th>
2026-09-18 11:42:11 +0700

    This is my second commit

---
+++ Hello1.java
@@ -0,0 +1,7 @@
+public class Hello1 {
+    public static void main(String[] args){
+        System.out.println("Hello PNRU");
+        System.out.println("Hello ARE U OK");
+        System.out.println("Hello SAWASDEEKA");
+    }
+}
